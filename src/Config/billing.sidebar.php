8973c330baf9a507cee9dacb7a5f5b82001f6d92
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-08-25 11:36:19 +0200

    refactor(sidebar): fix typo in SeAT syntax

--- src/Config/billing.sidebar.php
+++ src/Config/billing.sidebar.php
@@ -1,29 +1,27 @@
 <?php
 
 return [
-        'billing' => [
-                'name' => 'Seat IRS',
-                'icon' => 'fa-credit-card',
-                'route_segment' => 'billing',
-                'permission' => 'billing.view',
-                'route' => 'billing.view',
-
-        'entries'       => [
+    'billing' => [
+        'name' => 'SeAT IRS',
+        'icon' => 'fa-credit-card',
+        'route_segment' => 'billing',
+        'permission' => 'billing.view',
+        'route' => 'billing.view',
+        'entries' => [
             'billing' => [
                 'name' => 'Billing Statements',
                 'icon' => 'fa-money',
                 'route_segment' => 'billing',
                 'route' => 'billing.view',
-                'permission' => 'biling.view'
+                'permission' => 'biling.view',
             ],
             'settings' => [
                 'name' => 'Settings',
                 'icon' => 'fa-gear',
                 'route_segment' => 'billing',
                 'route' => 'billing.settings',
-                'permission' => 'billing.admin'
+                'permission' => 'billing.admin',
             ],
-
         ],
-        ],
+    ],
 ];
