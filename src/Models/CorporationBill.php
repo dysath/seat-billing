21c4608f3db57acc1b2353c2e19daf9bf70e579b
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-06-09 15:38:22 +0200

    fix(check): ensure paid bill are properly displayed

--- src/Models/CorporationBill.php
+++ src/Models/CorporationBill.php
@@ -47,9 +47,9 @@ class CorporationBill extends Model
         $mined_amount = $this->mining_bill * ($this->mining_modifier / 100) * ($this->mining_taxrate / 100);
         $searched_amount = $pve_amount + $mined_amount;
         $start_date = carbon(sprintf('%d-%d-01', $this->year, $this->month))->addMonth()->startOfMonth();
-        $end_date = $start_date->endOfMonth();
+        $end_date = carbon(sprintf('%d-%d-01', $this->year, $this->month))->addMonth()->endOfMonth();
 
-        $journal_id = $this->getPaidBillFromJournal($searched_amount, $start_date->toDateString(), $end_date->toDateString())
+        $journal_id = $this->getPaidBillFromJournal(round($searched_amount, 0), $start_date->toDateString(), $end_date->toDateString())
                            ->get();
 
         return ! $journal_id->isEmpty();
@@ -62,9 +62,9 @@ class CorporationBill extends Model
     {
         $searched_amount = $this->pve_bill * ($this->pve_taxrate / 100);
         $start_date = carbon(sprintf('%d-%d-01', $this->year, $this->month))->addMonth()->startOfMonth();
-        $end_date = $start_date->endOfMonth();
+        $end_date = carbon(sprintf('%d-%d-01', $this->year, $this->month))->addMonth()->endOfMonth();
 
-        $journal_id = $this->getPaidBillFromJournal($searched_amount, $start_date->toDateString(), $end_date->toDateString())
+        $journal_id = $this->getPaidBillFromJournal(round($searched_amount, 0), $start_date->toDateString(), $end_date->toDateString())
                            ->get();
 
         return ! $journal_id->isEmpty();
@@ -77,9 +77,9 @@ class CorporationBill extends Model
     {
         $searched_amount = $this->mining_bill * ($this->mining_modifier / 100) * ($this->mining_taxrate / 100);
         $start_date = carbon(sprintf('%d-%d-01', $this->year, $this->month))->addMonth()->startOfMonth();
-        $end_date = $start_date->endOfMonth();
+        $end_date = carbon(sprintf('%d-%d-01', $this->year, $this->month))->addMonth()->endOfMonth();
 
-        $journal_id = $this->getPaidBillFromJournal($searched_amount, $start_date->toDateString(), $end_date->toDateString())
+        $journal_id = $this->getPaidBillFromJournal(round($searched_amount, 0), $start_date->toDateString(), $end_date->toDateString())
                            ->get();
 
         return ! $journal_id->isEmpty();
@@ -91,11 +91,16 @@ class CorporationBill extends Model
      * @param string $end_date
      * @return mixed
      */
-    private function getPaidBillFromJournal(float $searched_amount, string $start_date, string $end_date)
+    private function getPaidBillFromJournal(int $searched_amount, string $start_date, string $end_date)
     {
-        return CorporationWalletJournal::where('amount', round($searched_amount, 2))
-            ->where('corporation_id', $this->corporation_id)
+        $min_amount = ($searched_amount + 1) * -1;
+        $max_amount = ($searched_amount - 1) * -1;
+
+        $query = CorporationWalletJournal::where('corporation_id', $this->corporation_id)
             ->whereIn('ref_type', ['player_donation', 'contract_price_payment_corp'])
-            ->whereBetween('date', [$start_date, $end_date]);
+            ->whereBetween('date', [$start_date, $end_date])
+            ->whereBetween('amount', [$min_amount, $max_amount]);
+
+        return $query;
     }
 }
